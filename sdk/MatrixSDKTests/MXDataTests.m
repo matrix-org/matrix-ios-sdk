58e874cf314ae01ed00c57f943dc6a4843ce56d1
Emmanuel ROHEE <erohee@amdocs.com>
2014-10-07 10:52:54 +0200

    Started tests of MXData with a test on recents

---
+++ sdk/MatrixSDKTests/MXDataTests.m
@@ -0,0 +1,81 @@
+/*
+ Copyright 2014 OpenMarket Ltd
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import "MXData.h"
+
+#define MX_HOMESERVER_URL @"http://matrix.org"
+
+// @TODO: Find an automatic way to test with an user account
+#define MX_USER_ID @"@your_name:matrig.org"
+#define MX_ACCESS_TOKEN @"Your access token"
+
+@interface MXDataTests : XCTestCase
+{
+    MXData *matrixData;
+}
+@end
+
+@implementation MXDataTests
+
+- (void)setUp
+{
+    [super setUp];
+
+    MXSession *matrixSession = [[MXSession alloc] initWithHomeServer:MX_HOMESERVER_URL
+                                                              userId:MX_USER_ID
+                                                         accessToken:MX_ACCESS_TOKEN];
+    
+    matrixData = [[MXData alloc] initWithMatrixSession:matrixSession];
+}
+
+- (void)tearDown
+{
+    [matrixData close];
+    matrixData = nil;
+    
+    [super tearDown];
+}
+
+- (void)testRecents
+{
+    XCTestExpectation *expectation = [self expectationWithDescription:@"asyncTest"];
+
+    [matrixData start:^{
+        
+        NSArray *recents = [matrixData recents];
+        
+        XCTAssert(0 < recents.count, @"There must be recents");
+        
+        for (MXEvent *event in recents)
+        {
+            XCTAssert(nil != event.event_id, @"The event must have an event_id to be valid");
+        }
+        
+        [expectation fulfill];
+        
+    } failure:^(NSError *error) {
+        
+        [expectation fulfill];
+        
+    }];
+    
+    [self waitForExpectationsWithTimeout:10000 handler:nil];
+}
+
+@end
