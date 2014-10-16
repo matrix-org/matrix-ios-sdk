837fc332ee5db56bb1ec9016390146166c077f42
manuroe <manu@matrix.org>
2014-10-16 15:53:15 +0200

    (Missing files) Added registration of events listeners at MXData level so that recents can be updated

---
+++ sdk/MatrixSDK/Data/MXDataEventListener.h
@@ -0,0 +1,60 @@
+/*
+ Copyright 2014 OpenMarket Ltd
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <UIKit/UIKit.h>
+
+#import "MXEventListener.h"
+
+@class MXData;
+@class MXRoomData;
+
+/**
+ Block called when an event of the registered types has been handled by the `MXRoomData` instance.
+ This is a specialisation of the `MXEventListenerBlock`.
+ 
+ @param roomData the `MXRoomData` that handled the event.
+ @param event the new event.
+ @param isLive YES if it is new event.
+ */
+typedef void (^MXDataEventListenerBlock)(MXData *matrixData, MXEvent *event, BOOL isLive);
+
+/**
+ The `MXDataEventListener` class stores information about a listener to MXData events
+ Such listener is called here global listener since it listens to all events and not the ones limited to a room.
+ */
+@interface MXDataEventListener : MXEventListener
+
+
+/**
+ Add a MXRoomData the MXDataEventListener must listen to events from.
+ 
+ @param roomData the MXRoomData to listen to.
+ */
+- (void)addRoomDataToSpy:(MXRoomData*)roomData;
+
+/**
+ Stop spying to a MXRoomData events.
+ 
+ @param roomData the MXRoomData to stop listening to.
+ */
+- (void)removeSpiedRoomData:(MXRoomData*)roomData;
+
+/**
+ Stop spying to all registered MXRoomDatas.
+ */
+- (void)removeAllSpiedRoomDatas;
+
+@end
---
+++ sdk/MatrixSDK/Data/MXDataEventListener.m
@@ -0,0 +1,78 @@
+/*
+ Copyright 2014 OpenMarket Ltd
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "MXDataEventListener.h"
+
+#import "MXData.h"
+#import "MXRoomData.h"
+
+@interface MXDataEventListener()
+{
+    // A global listener needs to listen to each MXRoomData new events
+    // roomDataEventListeners is the list of all MXRoomData listener for this MXDataEventListener
+    // The key is the room_id. The valuse, the registered MXEventListener of the MXRoomData
+    NSMutableDictionary *roomDataEventListeners;
+}
+@end
+
+@implementation MXDataEventListener
+
+- (instancetype)initWithSender:(id)sender andEventTypes:(NSArray *)eventTypes andListenerBlock:(MXEventListenerBlock)listenerBlock
+{
+    self = [super initWithSender:sender andEventTypes:eventTypes andListenerBlock:listenerBlock];
+    if (self)
+    {
+        roomDataEventListeners = [NSMutableDictionary dictionary];
+    }
+    return self;
+}
+
+- (void)addRoomDataToSpy:(MXRoomData*)roomData
+{
+    if (![roomDataEventListeners objectForKey:roomData.room_id])
+    {
+        roomDataEventListeners[roomData.room_id] =
+        [roomData registerEventListenerForTypes:self.eventTypes block:^(MXRoomData *roomData, MXEvent *event, BOOL isLive) {
+            self.listenerBlock(self.sender, event, isLive);
+        }];
+    }
+
+}
+
+- (void)removeSpiedRoomData:(MXRoomData*)roomData
+{
+    if ([roomDataEventListeners objectForKey:roomData.room_id])
+    {
+        [roomData unregisterListener:roomDataEventListeners[roomData.room_id]];
+        [roomDataEventListeners removeObjectForKey:roomData.room_id];
+    }
+}
+
+- (void)removeAllSpiedRoomDatas
+{
+    // Here sender is the MXData instance. @TODO: not nice
+    MXData *matrixData = (MXData *)self.sender;
+    
+    for (NSString *room_id in roomDataEventListeners)
+    {
+        MXRoomData *roomData = [matrixData getRoomData:room_id];
+        [roomData unregisterListener:roomDataEventListeners[roomData.room_id]];
+        
+    }
+    [roomDataEventListeners removeAllObjects];
+}
+
+@end
