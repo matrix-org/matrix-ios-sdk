8b8edc7dd3df6ef246512b6336eb42f4aaa93c5b
manuroe <manu@matrix.org>
2014-11-27 18:10:30 +0100

    Started MXMemoryStore. Not yet tested. It was created to copy/past the old code that managed MXRoom.messages

---
+++ sdk/MatrixSDK/Data/Store/MXMemoryStore/MXMemoryRoomStore.h
@@ -0,0 +1,56 @@
+/*
+ Copyright 2014 OpenMarket Ltd
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <Foundation/Foundation.h>
+
+#import "MXEventListener.h"
+
+@interface MXMemoryRoomStore : NSObject
+
+/**
+ Store room event received from the home server.
+
+ @param event the MXEvent object to store.
+ @param direction the origin of the event. Live or past events.
+ */
+- (void)storeEvent:(MXEvent*)event direction:(MXEventDirection)direction;
+
+/**
+ The current pagination token of the room.
+ */
+@property (nonatomic) NSString *paginationToken;
+
+/**
+ The flag indicating that the SDK has reached the end of pagination
+ in its pagination requests to the home server.
+ */
+@property (nonatomic) BOOL hasReachedHomeServerPaginationEnd;
+
+/**
+ Reset pagination mechanism in the room..
+
+ */
+- (void)resetPagination;
+
+/**
+ Get more messages in the room from the current pagination point.
+
+ @param numMessages the number or messages to get.
+ @return an array of time-ordered MXEvent objects. nil if no more are available.
+ */
+- (NSArray*)paginate:(NSUInteger)numMessages;
+
+@end
---
+++ sdk/MatrixSDK/Data/Store/MXMemoryStore/MXMemoryRoomStore.m
@@ -0,0 +1,91 @@
+/*
+ Copyright 2014 OpenMarket Ltd
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "MXMemoryRoomStore.h"
+
+@interface MXMemoryRoomStore ()
+{
+    // The events downloaded so far.
+    // The order is chronological: the first item is the oldest message.
+    NSMutableArray *messages;
+
+    // The last message of the room.
+    // @TODO: Is it required??
+    MXEvent *lastMessage;
+
+    // This is the position from the end
+    NSInteger paginationPosition;
+}
+
+@end
+
+@implementation MXMemoryRoomStore
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self)
+    {
+        messages = [NSMutableArray array];
+    }
+    return self;
+}
+
+- (void)storeEvent:(MXEvent *)event direction:(MXEventDirection)direction
+{
+    if (MXEventDirectionBackwards == direction)
+    {
+        [messages insertObject:event atIndex:0];
+    }
+    else
+    {
+        [messages addObject:event];
+        lastMessage = event;
+
+        // The messages array end has changed, shift the current pagination position
+        paginationPosition -= 1;
+    }
+}
+
+- (void)resetPagination
+{
+    paginationPosition = messages.count - 1;
+}
+
+- (NSArray *)paginate:(NSUInteger)numMessages
+{
+    NSArray *paginatedMessages;
+
+    if (0 < paginationPosition)
+    {
+        if (numMessages < paginationPosition)
+        {
+            // Return a slice of messages
+            paginatedMessages = [messages subarrayWithRange:NSMakeRange(paginationPosition - numMessages, numMessages)];
+            paginationPosition -= numMessages;
+        }
+        else
+        {
+            // Return the last slice of messages
+            paginatedMessages = [messages subarrayWithRange:NSMakeRange(0, paginationPosition)];
+            paginationPosition = 0;
+        }
+    }
+
+    return paginatedMessages;
+}
+
+@end
