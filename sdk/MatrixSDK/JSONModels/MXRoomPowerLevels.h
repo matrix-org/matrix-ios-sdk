9df5b51324977180c2120e27f3ac432fab8fa2b0
manuroe <manu@matrix.org>
2014-12-03 18:24:48 +0100

    Created MXRoomPowerLevels

---
+++ sdk/MatrixSDK/JSONModels/MXRoomPowerLevels.h
@@ -0,0 +1,93 @@
+/*
+ Copyright 2014 OpenMarket Ltd
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "MXJSONModel.h"
+
+#import "MXEvent.h"
+
+/**
+ `MXRoomPowerLevels` represents the content of a m.room.power_levels event.
+
+ Such event provides information of the power levels attributed to the room members.
+ It also defines minimum power level value a member must have to accomplish an action or 
+ to post an event of a given type.
+ */
+@interface MXRoomPowerLevels : MXJSONModel
+
+#pragma mark - Power levels of room members
+/**
+ The users who have a defined power level.
+ The dictionary keys are user ids and the values, their power levels
+ */
+@property (nonatomic) NSDictionary *users;
+
+/**
+ The default power level for users not listed in `users`.
+ */
+@property (nonatomic) NSUInteger usersDefault;
+
+/**
+ Get the power level of a member of the room.
+
+ @param userId The id to the user.
+ @return his power level.
+ */
+- (NSUInteger)powerLevelOfUserWithUserID:(NSString*)userId;
+
+
+#pragma mark - minimum power level for actions
+/**
+ The minimum power level to ban someone.
+ */
+@property (nonatomic) NSUInteger ban;
+
+/**
+ The minimum power level to kick someone.
+ */
+@property (nonatomic) NSUInteger kick;
+
+/**
+ The minimum power level to redact an event.
+ */
+@property (nonatomic) NSUInteger redact;
+
+
+#pragma mark - minimum power level for posting events
+/**
+ The event types for which a minimum power level has been defined.
+ The dictionary keys are event type and the values, their minimum power levels
+ */
+@property (nonatomic) NSDictionary *events;
+
+/**
+ The default minimum power level to post an event as a message when its event type is not defined in `events`.
+ */
+@property (nonatomic) NSUInteger eventsDefault;
+
+/**
+ The default minimum power level to post a non state event when its event type is not defined in `events`.
+ */
+@property (nonatomic) NSUInteger stateDefault;
+
+/**
+ Get the minimum power level the user must have to post an event of the given type.
+
+ @param eventType the type of event.
+ @return the required minimum power level.
+ */
+- (NSUInteger)minimumPowerLevelForEvent:(MXEventType)eventType;
+
+@end
---
+++ sdk/MatrixSDK/JSONModels/MXRoomPowerLevels.m
@@ -0,0 +1,80 @@
+/*
+ Copyright 2014 OpenMarket Ltd
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "MXRoomPowerLevels.h"
+
+#import "MXTools.h"
+
+@implementation MXRoomPowerLevels
+- (instancetype)init
+{
+    self = [super init];
+    if (self)
+    {
+        // Filled default values as specified by the doc
+        _usersDefault = 0;
+
+        // @TODO: are the following values still true as the doc is currently obsolete?
+        _eventsDefault = 50;
+        _stateDefault = 50;
+    }
+    return self;
+}
+
+- (NSUInteger)powerLevelOfUserWithUserID:(NSString *)userId
+{
+    // By default, use usersDefault
+    NSUInteger userPowerLevel = _usersDefault;
+
+    NSNumber *powerLevel = _users[userId];
+    if (powerLevel)
+    {
+        userPowerLevel = [powerLevel unsignedIntegerValue];
+    }
+
+    return userPowerLevel;
+}
+
+- (NSUInteger)minimumPowerLevelForEvent:(MXEventType)eventType
+{
+    NSUInteger minimumPowerLevel;
+
+    MXEventTypeString type = [MXTools eventTypeString:eventType];
+
+    NSNumber *powerLevel = _events[type];
+    if (powerLevel)
+    {
+        minimumPowerLevel = [powerLevel unsignedIntegerValue];
+    }
+
+    // Use the default value according to the type of event.
+    // The Matrix protocol allows to post a state event both as a state event and as a message.
+    // But currently, there is no such event type that has this behavior.
+    // Only m.room.message event are posted as message events. All other events are posted as state event.
+    // So, for now, do not manage this protocol open door.
+    else if (MXEventTypeRoomMessage == eventType)
+    {
+        minimumPowerLevel = _eventsDefault;
+    }
+    else
+    {
+        minimumPowerLevel = _stateDefault;
+    }
+
+    return minimumPowerLevel;
+}
+
+@end
