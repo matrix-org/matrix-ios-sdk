4dc228a8a33deaa1a4d330d5a322ebe9fb505e6c
manuroe <manu@matrix.org>
2014-12-22 17:29:22 +0100

    Increased store version to force cleaning at startup in order flush typing notifications events.

--- sdk/MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
+++ sdk/MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
@@ -20,7 +20,7 @@
 
 #import "MXFileStoreMetaData.h"
 
-NSUInteger const kMXFileVersion = 1;
+NSUInteger const kMXFileVersion = 2;
 
 NSString *const kMXFileStoreFolder = @"MXFileStore";
 NSString *const kMXFileStoreMedaDataFile = @"MXFileStore";
