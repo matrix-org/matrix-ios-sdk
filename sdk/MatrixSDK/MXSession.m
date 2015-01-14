34ed1caacc5b07c166fe91b6fe03c735613510ae
manuroe <manu@matrix.org>
2015-01-14 13:26:49 +0100

    Prepare 0.2.1 release

--- sdk/MatrixSDK/MXSession.m
+++ sdk/MatrixSDK/MXSession.m
@@ -27,7 +27,7 @@
 /**
  The Matrix iOS SDK version.
  */
-const NSString *MatrixSDKVersion = @"0.2.0";
+const NSString *MatrixSDKVersion = @"0.2.1";
 
 /**
  Default timeouts used by the events streams.
