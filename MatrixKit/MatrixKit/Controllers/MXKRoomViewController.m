c6f9e4bba81f5d90014859130abdd5a10b3ace24
manuroe <manu@matrix.org>
2015-03-04 10:30:17 +0100

    Started MatrixKit

---
+++ MatrixKit/MatrixKit/Controllers/MXKRoomViewController.m
@@ -0,0 +1,47 @@
+/*
+ Copyright 2015 OpenMarket Ltd
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "MXKRoomViewController.h"
+
+@interface MXKRoomViewController () {
+
+    UITableView *tableView;
+    MXKRoomDataSource *dataSource;
+}
+@end
+
+@implementation MXKRoomViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+    dataSource = [[MXKRoomDataSource alloc] init];
+
+    tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+
+    tableView.dataSource = dataSource;
+    tableView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:tableView];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+    // Dispose of any resources that can be recreated.
+}
+
+@end
