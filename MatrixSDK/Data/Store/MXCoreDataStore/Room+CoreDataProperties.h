bca296d8973a02784701f4ba27cd920de84fa8f3
manuroe <manu@matrix.org>
2015-10-16 15:58:21 +0200

    MXCoreData: Fixed crashes in CoreData CoreDataGeneratedAccessors automatically generated methods

--- MatrixSDK/Data/Store/MXCoreDataStore/Room+CoreDataProperties.h
+++ MatrixSDK/Data/Store/MXCoreDataStore/Room+CoreDataProperties.h
@@ -1,16 +1,20 @@
-//
-//  Room+CoreDataProperties.h
-//  MatrixSDK
-//
-//  Created by Emmanuel ROHEE on 14/10/15.
-//  Copyright © 2015 matrix.org. All rights reserved.
-//
-//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
-//  to delete and recreate this implementation file for your updated model.
-//
+/*
+ Copyright 2015 OpenMarket Ltd
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 #import "Room.h"
-#import "MXEventEntity.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -19,21 +23,18 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nullable, nonatomic, retain) NSNumber *hasReachedHomeServerPaginationEnd;
 @property (nullable, nonatomic, retain) NSString *paginationToken;
 @property (nullable, nonatomic, retain) NSString *roomId;
-
-// The events downloaded so far.
-// The order is chronological: the first item is the oldest message.
 @property (nullable, nonatomic, retain) NSOrderedSet<MXEventEntity *> *messages;
-
 @property (nullable, nonatomic, retain) NSSet<MXEventEntity *> *state;
+@property (nullable, nonatomic, retain) Account *account;
 
 @end
 
 @interface Room (CoreDataGeneratedAccessors)
 
-- (void)insertObject:(NSManagedObject *)value inMessagesAtIndex:(NSUInteger)idx;
+- (void)insertObject:(MXEventEntity *)value inMessagesAtIndex:(NSUInteger)idx;
 - (void)removeObjectFromMessagesAtIndex:(NSUInteger)idx;
 - (void)insertMessages:(NSArray<MXEventEntity *> *)value atIndexes:(NSIndexSet *)indexes;
-- (void)removeMessagesAtIndexes:(MXEventEntity *)indexes;
+- (void)removeMessagesAtIndexes:(NSIndexSet *)indexes;
 - (void)replaceObjectInMessagesAtIndex:(NSUInteger)idx withObject:(MXEventEntity *)value;
 - (void)replaceMessagesAtIndexes:(NSIndexSet *)indexes withMessages:(NSArray<MXEventEntity *> *)values;
 - (void)addMessagesObject:(MXEventEntity *)value;
