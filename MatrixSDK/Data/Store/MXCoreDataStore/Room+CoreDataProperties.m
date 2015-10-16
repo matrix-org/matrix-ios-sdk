8252f58fb9804224c7a324ea41af96b3a5256490
manuroe <manu@matrix.org>
2015-10-16 15:58:21 +0200

    MXCoreData: Fixed crashes in CoreData CoreDataGeneratedAccessors automatically generated methods

--- MatrixSDK/Data/Store/MXCoreDataStore/Room+CoreDataProperties.m
+++ MatrixSDK/Data/Store/MXCoreDataStore/Room+CoreDataProperties.m
@@ -1,15 +1,22 @@
-//
-//  Room+CoreDataProperties.m
-//  MatrixSDK
-//
-//  Created by Emmanuel ROHEE on 14/10/15.
-//  Copyright © 2015 matrix.org. All rights reserved.
-//
-//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
-//  to delete and recreate this implementation file for your updated model.
-//
+/*
+ Copyright 2015 OpenMarket Ltd
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 #import "Room+CoreDataProperties.h"
+
+static NSString *const kItemsKey = @"messages";
 
 @implementation Room (CoreDataProperties)
 
@@ -18,5 +25,40 @@
 @dynamic roomId;
 @dynamic messages;
 @dynamic state;
+@dynamic account;
+
+// There is an old bug on core data and NSOrderedSet with one to many objects relationships
+// where CoreDataGeneratedAccessors methods crash.
+// The best workaround is to rewrite them (@see http://stackoverflow.com/questions/7385439/exception-thrown-in-nsorderedset-generated-accessors/9556747#9556747)
+- (void)insertObject:(MXEventEntity *)value inMessagesAtIndex:(NSUInteger)idx
+{
+    NSIndexSet* indexes = [NSIndexSet indexSetWithIndex:idx];
+    [self willChange:NSKeyValueChangeInsertion valuesAtIndexes:indexes forKey:kItemsKey];
+    NSMutableOrderedSet *tmpOrderedSet = [NSMutableOrderedSet orderedSetWithOrderedSet:[self mutableOrderedSetValueForKey:kItemsKey]];
+    //NSLog(@"Before: %@", tmpOrderedSet);
+    [tmpOrderedSet insertObject:value atIndex:idx];
+    //NSLog(@"After: %@", tmpOrderedSet);
+    [self setPrimitiveValue:tmpOrderedSet forKey:kItemsKey];
+    [self didChange:NSKeyValueChangeInsertion valuesAtIndexes:indexes forKey:kItemsKey];
+}
+
+- (void)removeObjectFromMessagesAtIndex:(NSUInteger)idx {
+    NSIndexSet* indexes = [NSIndexSet indexSetWithIndex:idx];
+    [self willChange:NSKeyValueChangeRemoval valuesAtIndexes:indexes forKey:kItemsKey];
+    NSMutableOrderedSet *tmpOrderedSet = [NSMutableOrderedSet orderedSetWithOrderedSet:[self mutableOrderedSetValueForKey:kItemsKey]];
+    [tmpOrderedSet removeObjectAtIndex:idx];
+    [self setPrimitiveValue:tmpOrderedSet forKey:kItemsKey];
+    [self didChange:NSKeyValueChangeRemoval valuesAtIndexes:indexes forKey:kItemsKey];
+}
+
+- (void)addMessagesObject:(MXEventEntity *)value {
+    NSMutableOrderedSet *tmpOrderedSet = [NSMutableOrderedSet orderedSetWithOrderedSet:[self mutableOrderedSetValueForKey:kItemsKey]];
+    NSUInteger idx = [tmpOrderedSet count];
+    NSIndexSet* indexes = [NSIndexSet indexSetWithIndex:idx];
+    [self willChange:NSKeyValueChangeInsertion valuesAtIndexes:indexes forKey:kItemsKey];
+    [tmpOrderedSet addObject:value];
+    [self setPrimitiveValue:tmpOrderedSet forKey:kItemsKey];
+    [self didChange:NSKeyValueChangeInsertion valuesAtIndexes:indexes forKey:kItemsKey];
+}
 
 @end
