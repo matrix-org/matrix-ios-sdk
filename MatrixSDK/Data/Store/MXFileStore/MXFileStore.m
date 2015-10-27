c09ccf19c690f453ea26f1504fc9160373db194d
giomfo <giom@matrix.org>
2015-10-27 12:56:51 +0100

    MXFileStore: update kMXFileVersion value as the file structure may have changed

--- MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
+++ MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
@@ -20,7 +20,7 @@
 
 #import "MXFileStoreMetaData.h"
 
-NSUInteger const kMXFileVersion = 9;
+NSUInteger const kMXFileVersion = 10;
 
 NSString *const kMXFileStoreFolder = @"MXFileStore";
 NSString *const kMXFileStoreMedaDataFile = @"MXFileStore";
