3a8b98ad6122a34a2a7e631476836929a111fbbe
manuroe <manu@matrix.org>
2015-11-27 13:54:23 +0100

    MXFileStore: version++

--- MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
+++ MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
@@ -20,7 +20,7 @@
 
 #import "MXFileStoreMetaData.h"
 
-NSUInteger const kMXFileVersion = 11;
+NSUInteger const kMXFileVersion = 12;
 
 NSString *const kMXFileStoreFolder = @"MXFileStore";
 NSString *const kMXFileStoreMedaDataFile = @"MXFileStore";
