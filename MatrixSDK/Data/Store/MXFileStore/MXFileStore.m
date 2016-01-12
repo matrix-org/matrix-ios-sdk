9dee2382581390bc1ecf485cadf1251dc2e569cb
giomfo <giom@matrix.org>
2016-01-12 16:57:03 +0100

    MXFileStore: Force refresh
    Force refresh to take into account server api changes on event description.

--- MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
+++ MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
@@ -20,7 +20,7 @@
 
 #import "MXFileStoreMetaData.h"
 
-NSUInteger const kMXFileVersion = 14;
+NSUInteger const kMXFileVersion = 15;
 
 NSString *const kMXFileStoreFolder = @"MXFileStore";
 NSString *const kMXFileStoreMedaDataFile = @"MXFileStore";
