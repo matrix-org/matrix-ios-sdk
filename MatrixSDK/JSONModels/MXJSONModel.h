matrix-org/matrix-ios-sdk

commit cd61dd5bdbe491da4de78f16cf29ee751b7e621c

JSON validation: Log errors in release build too (but do not break)

MatrixSDK/JSONModels/MXJSONModel.h
@@ -147,4 +147,7 @@
 /**
  Log a validation error.
  */
-#define MXJSONModelSetLogError(theClass, value)   NSAssert(NO, @"[MXJSONModelSet] Error: Unexpected type for parsing at %@:%d. Expected: %@. Got: %@ (%@)", @(__FILE__).lastPathComponent, __LINE__, theClass, value, NSStringFromClass([value class]))
+#define MXJSONModelSetLogError(theClass, value) \
+    NSLog(@"[MXJSONModelSet] Error: Unexpected type for parsing at %@:%d. Expected: %@. Got: %@ (%@)", \
+            @(__FILE__).lastPathComponent, __LINE__, theClass, value, NSStringFromClass([value class])); \
+    NSAssert(NO, @"")

commit 67c3261d787ca746c0f86f121d38eeaf4b1d2f71

Fix regression introduced in cd61dd5bdbe491da4de78f16cf29ee751b7e621c

MatrixSDK/JSONModels/MXJSONModel.h
@@ -84,7 +84,7 @@
     if (value) \
     { \
         if ([value isKindOfClass:class]) var = value; \
-        else MXJSONModelSetLogError(class, value);  \
+        else MXJSONModelSetLogError(class, value)  \
     }
 
 /**
@@ -113,7 +113,7 @@
     if (value) \
     { \
         if ([value isKindOfClass:NSNumber.class]) var = [((NSNumber*)value) method]; \
-        else MXJSONModelSetLogError(NSNumber.class, value);  \
+        else MXJSONModelSetLogError(NSNumber.class, value)  \
     }
 
 /**
@@ -127,7 +127,7 @@
     if (value) \
     { \
         if ([value isKindOfClass:NSDictionary.class]) var = [MXJSONModelClass modelFromJSON:value]; \
-        else MXJSONModelSetLogError(NSDictionary.class, value);  \
+        else MXJSONModelSetLogError(NSDictionary.class, value)  \
     }
 
 /**
@@ -141,13 +141,15 @@
     if (values) \
     { \
         if ([values isKindOfClass:NSArray.class]) var = [MXJSONModelClass modelsFromJSON:values]; \
-        else MXJSONModelSetLogError(NSArray.class, values);  \
+        else MXJSONModelSetLogError(NSArray.class, values)  \
     }
 
 /**
  Log a validation error.
  */
 #define MXJSONModelSetLogError(theClass, value) \
-    NSLog(@"[MXJSONModelSet] Error: Unexpected type for parsing at %@:%d. Expected: %@. Got: %@ (%@)", \
+    { \
+        NSLog(@"[MXJSONModelSet] Error: Unexpected type for parsing at %@:%d. Expected: %@. Got: %@ (%@)", \
             @(__FILE__).lastPathComponent, __LINE__, theClass, value, NSStringFromClass([value class])); \
-    NSAssert(NO, @"")
+        NSAssert(NO, @""); \
+    }