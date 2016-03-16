9f6c1fdb3f8e3dfdaf3c675fe5ae66dfef3d51ac
giomfo <giom@matrix.org>
2016-03-16 11:03:55 +0100

    MXSessionTests: Fix test on tagged rooms order

--- MatrixSDKTests/MXSessionTests.m
+++ MatrixSDKTests/MXSessionTests.m
@@ -825,9 +825,9 @@
                                     // Room ordering: a tagged room with no order value must have higher priority
                                     // than the tagged rooms with order value.
 
-                                    XCTAssertEqualObjects(room1.state.topic, @"3", "The order is wrong");
-                                    XCTAssertEqualObjects(room2.state.topic, @"1", "The order is wrong");
-                                    XCTAssertEqualObjects(room3.state.topic, @"2", "The order is wrong");
+                                    XCTAssertEqualObjects(room1.state.topic, @"1", "The order is wrong");
+                                    XCTAssertEqualObjects(room2.state.topic, @"2", "The order is wrong");
+                                    XCTAssertEqualObjects(room3.state.topic, @"3", "The order is wrong");
 
 
                                     // By the way, check roomsWithTag
@@ -864,7 +864,7 @@
 
 - (void)testRoomByTagsOrderWithStringTagOrder
 {
-    [self doRoomByTagsOrderTest:self withOrder1:@"apples" order2:@"oranges" order3:nil];
+    [self doRoomByTagsOrderTest:self withOrder1:nil order2:@"apples" order3:@"oranges"];
 }
 
 - (void)testRoomByTagsOrderWithFloatTagOrder
