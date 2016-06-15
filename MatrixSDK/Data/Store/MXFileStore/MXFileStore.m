f389c1982b1602f2d0f16736ee2b3dd99dfb620f
giomfo <giom@matrix.org>
2016-06-15 12:24:51 +0200

    MXRoomPowerLevels: If the room contains no power_levels event, the state_default is 0. The events_default is 0 in either of these cases.
    But, state_default defaults to 50 if there is a power level event but no states_default key.
    
    + Support legacy CamelCase keys.
    + Force file store flush.

--- MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
+++ MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
@@ -20,7 +20,7 @@
 
 #import "MXFileStoreMetaData.h"
 
-NSUInteger const kMXFileVersion = 27;
+NSUInteger const kMXFileVersion = 28;
 
 NSString *const kMXFileStoreFolder = @"MXFileStore";
 NSString *const kMXFileStoreMedaDataFile = @"MXFileStore";
