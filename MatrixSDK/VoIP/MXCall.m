ad36afcf1973e9189d824f921936db7afc3668f3
manuroe <manu@matrix.org>
2016-08-17 17:18:01 +0200

    MXCall: Fix MXCall.isConferenceCall which must be true when the joined members count is STRICTLY more than 2

--- MatrixSDK/VoIP/MXCall.m
+++ MatrixSDK/VoIP/MXCall.m
@@ -91,7 +91,7 @@ NSString *const kMXCallStateDidChange = @"kMXCallStateDidChange";
         _state = MXCallStateFledgling;
 
         // Consider we are using a conference call when there are more than 2 users
-        _isConferenceCall = (2 <= _room.state.joinedMembers.count);
+        _isConferenceCall = (2 < _room.state.joinedMembers.count);
 
         localICECandidates = [NSMutableArray array];
 
