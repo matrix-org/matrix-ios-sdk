95890863aa087606f3014ff1794abd1c3b65652c
manuroe <manu@matrix.org>
2016-08-24 16:03:22 +0200

    MXEventsEnumerator: Add new files to MatrixSDK.h

--- MatrixSDK/MatrixSDK.h
+++ MatrixSDK/MatrixSDK.h
@@ -31,6 +31,9 @@ FOUNDATION_EXPORT NSString *MatrixSDKVersion;
 #import <MatrixSDK/MXFileStore.h>
 #import <MatrixSDK/MXCoreDataStore.h>
 
+#import <MatrixSDK/MXEventsEnumeratorOnArray.h>
+#import <MatrixSDK/MXEventsByTypesEnumeratorOnArray.h>
+
 #import <MatrixSDK/MXLogger.h>
 
 #import "MXTools.h"
