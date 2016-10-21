627280f21e2a94a34b4f8792835acea779c69d5c
giomfo <giom@matrix.org>
2016-10-21 17:08:05 +0200

    Logs Improvement: HTTP errors are logged by MXHTTPClient. Remove this error from other logs.

--- MatrixSDK/MXSession.m
+++ MatrixSDK/MXSession.m
@@ -825,7 +825,7 @@ typedef void (^MXOnResumeDone)();
             MXError *mxError = [[MXError alloc] initWithNSError:error];
             if ([mxError.errcode isEqualToString:kMXErrCodeStringUnknownToken])
             {
-                NSLog(@"[MXSession] The access token is no more valid. Go to MXSessionStateUnknownToken state. Error: %@", error);
+                NSLog(@"[MXSession] The access token is no more valid. Go to MXSessionStateUnknownToken state.");
                 [self setState:MXSessionStateUnknownToken];
 
                 // Do nothing more because without a valid access_token, the session is useless
