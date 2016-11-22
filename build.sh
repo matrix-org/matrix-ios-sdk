eb4811e2f03865fa970a872318f4bd12871bfe7e
manuroe <manu@matrix.org>
2016-11-22 16:18:39 +0100

    Jenkins: Make it run CocoaPods lint

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ set -x
 pod update
 
 xcodebuild -workspace MatrixSDK.xcworkspace/ -scheme MatrixSDK -sdk iphonesimulator  -destination 'name=iPhone 4s'
+
+# Run CocoaPod lint in order to check pod submission
+pod spec lint
