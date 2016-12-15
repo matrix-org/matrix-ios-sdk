80a17acb374b3540659c75adfcbcd80859495ada
manuroe <manu@matrix.org>
2016-12-15 08:57:49 +0100

    build scripts: Move to iPhone 5s

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -x
 
 pod update
 
-xcodebuild -workspace MatrixSDK.xcworkspace/ -scheme MatrixSDK -sdk iphonesimulator  -destination 'name=iPhone 4s'
+xcodebuild -workspace MatrixSDK.xcworkspace/ -scheme MatrixSDK -sdk iphonesimulator  -destination 'name=iPhone 5s'
 
 # Run CocoaPod lint in order to check pod submission
 pod lib lint
--- test.sh
+++ test.sh
@@ -32,5 +32,5 @@ cd synapse/demo
 
 echo `date "+%F %T"` - start xcode
 cd ../..
-xcodebuild -workspace MatrixSDK.xcworkspace/ -scheme MatrixSDK -sdk iphonesimulator  -destination 'name=iPhone 4s' test
+xcodebuild -workspace MatrixSDK.xcworkspace/ -scheme MatrixSDK -sdk iphonesimulator  -destination 'name=iPhone 5s' test
 echo `date "+%F %T"` - xcode ended
