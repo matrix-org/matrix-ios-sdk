01b55abbcc4fc3c4eab5835a816114a6e5bf3964
manuroe <manu@matrix.org>
2017-06-14 16:00:07 +0200

    Jenkins: Try to make the good old Flywheel happy again

--- build.sh
+++ build.sh
@@ -7,5 +7,8 @@ pod update
 
 xcodebuild -workspace MatrixSDK.xcworkspace/ -scheme MatrixSDK -sdk iphonesimulator  -destination 'name=iPhone 5s'
 
+# Dirty patch to make the Jenkins iOS builder happy (this mac is too old to install xcode 8.3)
+echo "3.0" > .swift-version
+
 # Run CocoaPod lint in order to check pod submission
 pod lib lint
