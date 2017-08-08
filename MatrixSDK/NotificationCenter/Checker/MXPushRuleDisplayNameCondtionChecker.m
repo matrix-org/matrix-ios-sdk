matrix-org/matrix-ios-sdk

commit 4ea62234c0bde9f5ba575253780dc10541966ff2

Bug Fix: Getting notifications for unrelated messages vector-im/riot-android#1407

MatrixSDK/NotificationCenter/Checker/MXPushRuleDisplayNameCondtionChecker.m
@@ -59,7 +59,7 @@
             {
                 if (!userNameRegex)
                 {
-                    userNameRegex = [NSRegularExpression regularExpressionWithPattern:[NSString stringWithFormat:@"\\b%@\\b", mxSession.myUser.displayname] options:NSRegularExpressionCaseInsensitive error:nil];
+                    userNameRegex = [NSRegularExpression regularExpressionWithPattern:[NSString stringWithFormat:@"\\b\\Q%@\\E\\b", mxSession.myUser.displayname] options:NSRegularExpressionCaseInsensitive error:nil];
                 }
 
                 NSRange range = [userNameRegex rangeOfFirstMatchInString:body options:0 range:NSMakeRange(0, body.length)];

commit a5fa02a7b2544bd22af0d074ed7b603c74c83519

Bug Fix: Mentions do not work for names that start or end with a non-word character (like '[', ']', '@'...).
This regression was introduced by https://github.com/matrix-org/matrix-ios-sdk/commit/4ea62234c0bde9f5ba575253780dc10541966ff2.

MatrixSDK/NotificationCenter/Checker/MXPushRuleDisplayNameCondtionChecker.m
@@ -1,5 +1,6 @@
 /*
  Copyright 2015 OpenMarket Ltd
+ Copyright 2017 Vector Creations Ltd
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -26,6 +27,11 @@
      Regex for finding the user's display name in events content.
      */
     NSRegularExpression *userNameRegex;
+    
+    /**
+     The user display name used to build the regex.
+     */
+    NSString *currentUserName;
 }
 
 @end
@@ -38,6 +44,7 @@
     if (self)
     {
         mxSession = mxSession2;
+        currentUserName = nil;
     }
     return self;
 }
@@ -58,9 +65,10 @@
             
             if (body)
             {
-                if (!userNameRegex)
+                if (!userNameRegex || ![currentUserName isEqualToString:mxSession.myUser.displayname])
                 {
-                    userNameRegex = [NSRegularExpression regularExpressionWithPattern:[NSString stringWithFormat:@"\\b\\Q%@\\E\\b", mxSession.myUser.displayname] options:NSRegularExpressionCaseInsensitive error:nil];
+                    userNameRegex = [NSRegularExpression regularExpressionWithPattern:[NSString stringWithFormat:@"(^|\\W)\\Q%@\\E($|\\W)", mxSession.myUser.displayname] options:NSRegularExpressionCaseInsensitive error:nil];
+                    currentUserName = mxSession.myUser.displayname;
                 }
 
                 NSRange range = [userNameRegex rangeOfFirstMatchInString:body options:0 range:NSMakeRange(0, body.length)];