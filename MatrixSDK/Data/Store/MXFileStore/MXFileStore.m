750d7cae713f4642cb604ff1dc037429c979bee5
Giom Foret <giom@matrix.org>
2017-09-15 09:08:43 +0200

    MXFileStore: Clear store and reload
    see https://github.com/vector-im/riot-ios/pull/1510

--- MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
+++ MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
@@ -23,7 +23,7 @@
 #import "MXFileStoreMetaData.h"
 #import "MXSDKOptions.h"
 
-static NSUInteger const kMXFileVersion = 44;
+static NSUInteger const kMXFileVersion = 45;
 
 static NSString *const kMXFileStoreFolder = @"MXFileStore";
 static NSString *const kMXFileStoreMedaDataFile = @"MXFileStore";
