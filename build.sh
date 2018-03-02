b9559c84e76590994dd4d8185ace30bb13070cf4
manuroe <manu@matrix.org>
2018-03-02 17:56:14 +0100

    SwiftMatrixSDK: Force `lib lint` run on last Swift version (4.1)

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ pod update
 
 xcodebuild -workspace MatrixSDK.xcworkspace/ -scheme MatrixSDK -sdk iphonesimulator  -destination 'name=iPhone 5s'
 
-# Dirty patch to make the Jenkins iOS builder happy (this mac is too old to install xcode 8.3)
-echo "3.0" > .swift-version
+# Force lib lint on last Swift version
+echo "4.1" > .swift-version
 
 # Run CocoaPod lint in order to check pod submission
 # Note: --allow-warnings should be removed once we fix our warnings with CallKit stuff only available from iOS10
