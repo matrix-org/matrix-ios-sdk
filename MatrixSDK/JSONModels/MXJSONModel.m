0d84c688052a4f6702dad2670179662ec59b83e7
manuroe <manu@matrix.org>
2018-08-24 16:19:27 +0200

    Manage m.server_notice empty tag sent due to a bug server side

--- MatrixSDK/JSONModels/MXJSONModel.m
+++ MatrixSDK/JSONModels/MXJSONModel.m
@@ -1,6 +1,7 @@
 /*
  Copyright 2014 OpenMarket Ltd
- 
+ Copyright 2018 New Vector Ltd
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
@@ -63,7 +64,15 @@
                dictionary = [NSMutableDictionary dictionaryWithDictionary:JSONDictionary];
             }
 
-            [dictionary removeObjectForKey:key];
+            if ([key isEqualToString:kMXRoomTagServerNotice])
+            {
+                // Use an empty dict
+                dictionary[key] = @{};
+            }
+            else
+            {
+                [dictionary removeObjectForKey:key];
+            }
         }
         else if ([value isKindOfClass:[NSDictionary class]])
         {
