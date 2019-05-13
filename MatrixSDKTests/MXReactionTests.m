97517688ef0b5aa3c1f15e132a4592cfbeaa175d
manuroe <manu@matrix.org>
2019-05-13 17:07:50 +0200

    Aggregations: More tests

--- MatrixSDKTests/MXReactionTests.m
+++ MatrixSDKTests/MXReactionTests.m
@@ -19,6 +19,10 @@
 #import "MatrixSDKTestsData.h"
 
 #import "MXFileStore.h"
+#import "MXEventUnsignedData.h"
+#import "MXEventRelations.h"
+#import "MXEventAnnotationChunk.h"
+#import "MXEventAnnotation.h"
 
 @interface MXReactionTests : XCTestCase
 {
@@ -41,31 +45,97 @@
     matrixSDKTestsData = nil;
 }
 
-- (void)testReactionSendAndReceive
+// Create a room with an event with a reaction on it
+- (void)createScenario:(void(^)(MXSession *mxSession, MXRoom *room, XCTestExpectation *expectation, NSString *eventId, NSString *reactionEventId))readyToTest
 {
     [matrixSDKTestsData doMXSessionTestWithBobAndARoom:self andStore:[[MXMemoryStore alloc] init] readyToTest:^(MXSession *mxSession, MXRoom *room, XCTestExpectation *expectation) {
 
         [room sendTextMessage:@"Hello" success:^(NSString *eventId) {
 
-            [room sendReactionToEvent:eventId reaction:@"👍" success:^(NSString *eventId) {
-                XCTAssertNotNil(eventId);
+            [room sendReactionToEvent:eventId reaction:@"👍" success:^(NSString *reactionEventId) {
+
+                readyToTest(mxSession, room, expectation, eventId, reactionEventId);
 
             } failure:^(NSError *error) {
-                XCTFail(@"The operation should not fail - NSError: %@", error);
+                XCTFail(@"Cannot set up intial test conditions - error: %@", error);
                 [expectation fulfill];
             }];
-
         } failure:^(NSError *error) {
             XCTFail(@"Cannot set up intial test conditions - error: %@", error);
-            [expectation fulfill];
-        }];
-
-        [room listenToEventsOfTypes:@[kMXEventTypeStringReaction] onEvent:^(MXEvent *event, MXTimelineDirection direction, MXRoomState *roomState) {
-
             [expectation fulfill];
         }];
     }];
 }
 
+// - Send a message
+// - React on it
+// -> a m.reaction message must appear in the timeline
+- (void)testReactionSendAndReceive
+{
+    [matrixSDKTestsData doMXSessionTestWithBobAndARoom:self andStore:[[MXMemoryStore alloc] init] readyToTest:^(MXSession *mxSession, MXRoom *room, XCTestExpectation *expectation) {
+
+        // - Send a message
+        [room sendTextMessage:@"Hello" success:^(NSString *eventId) {
+
+            // - React on it
+            [room sendReactionToEvent:eventId reaction:@"👍" success:^(NSString *eventId) {
+                XCTAssertNotNil(eventId);
+            } failure:^(NSError *error) {
+                XCTFail(@"The operation should not fail - NSError: %@", error);
+                [expectation fulfill];
+            }];
+
+            [room listenToEventsOfTypes:@[kMXEventTypeStringReaction] onEvent:^(MXEvent *event, MXTimelineDirection direction, MXRoomState *roomState) {
+
+                // -> a m.reaction message must appear in the timeline
+                XCTAssertEqual(event.eventType, MXEventTypeReaction);
+                XCTAssertEqualObjects(event.type, kMXEventTypeStringReaction);
+
+                XCTAssertNotNil(event.relatesTo);
+                XCTAssertEqualObjects(event.relatesTo.relationType, MXEventRelationTypeAnnotation);
+                XCTAssertEqualObjects(event.relatesTo.eventId, eventId);
+                XCTAssertEqualObjects(event.relatesTo.key, @"👍");
+
+                [expectation fulfill];
+            }];
+        } failure:^(NSError *error) {
+            XCTFail(@"Cannot set up intial test conditions - error: %@", error);
+            [expectation fulfill];
+        }];
+    }];
+}
+
+// - Run the scenario
+// - Do an initial sync
+// -> The aggregated reactions count must be right
+- (void)testAggregationFromInitialSync
+{
+    // - Run the scenario
+    [self createScenario:^(MXSession *mxSession, MXRoom *room, XCTestExpectation *expectation, NSString *eventId, NSString *reactionEventId) {
+
+        // - Do an initial sync
+        [matrixSDKTestsData relogUserSession:mxSession withPassword:MXTESTS_BOB_PWD onComplete:^(MXSession *newSession) {
+
+            [newSession eventWithEventId:eventId inRoom:room.roomId success:^(MXEvent *event) {
+
+                // -> The aggregated reactions count must be right
+                XCTAssertNotNil(event.unsignedData.relations.annotation);
+
+                XCTAssertEqual(event.unsignedData.relations.annotation.chunk.count, 1);
+
+                MXEventAnnotation *annotation = event.unsignedData.relations.annotation.chunk.firstObject;
+                XCTAssertEqualObjects(annotation.type, MXEventAnnotationReaction);
+                XCTAssertEqualObjects(annotation.key, @"👍");
+                XCTAssertEqual(annotation.count, 1);
+
+                [expectation fulfill];
+
+            } failure:^(NSError *error) {
+                XCTFail(@"Cannot set up intial test conditions - error: %@", error);
+                [expectation fulfill];
+            }];
+        }];
+    }];
+}
 
 @end
