132fe3f91b6dbf9aa2730d9689f8acf6ae9d347b
manuroe <manu@matrix.org>
2019-06-12 17:46:12 +0200

    Reactions local echo: MXAggregatedReactions: add aggregatedReactionsWithNonZeroCount method
    Now we can return reactionCount with count = 0 (which reflects a pending reaction removal), this method can be used by the app to filter zero counts out

--- MatrixSDK/Aggregations/Data/MXAggregatedReactions.h
+++ MatrixSDK/Aggregations/Data/MXAggregatedReactions.h
@@ -27,6 +27,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic) NSArray<MXReactionCount*> *reactions;
 
+- (nullable MXAggregatedReactions *)aggregatedReactionsWithNonZeroCount;
+
 @end
 
 NS_ASSUME_NONNULL_END
