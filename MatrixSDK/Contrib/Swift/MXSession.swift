4cb75743ebbe3ea111a3eff27d0d160f5bc90629
manuroe <manuroe@users.noreply.github.com>
2020-01-28 12:55:49 +0100

    Update MatrixSDK/Contrib/Swift/MXSession.swift
    Co-Authored-By: giomfo <giom@matrix.org>

--- MatrixSDK/Contrib/Swift/MXSession.swift
+++ MatrixSDK/Contrib/Swift/MXSession.swift
@@ -146,7 +146,7 @@ public extension MXSession {
      - parameters:
      - parameters: The parameters for room creation.
      - completion: A block object called when the operation completes.
-     - response: Provides a MXCreateRoomResponse object on success.
+        - response: Provides a MXRoom object on success.
 
      - returns: a `MXHTTPOperation` instance.
      */
