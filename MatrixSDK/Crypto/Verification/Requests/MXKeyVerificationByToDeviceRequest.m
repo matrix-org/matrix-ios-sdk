ddde1eff30c14e18f8c0202311a0b49a3529685c
manuroe <manu@matrix.org>
2020-02-19 14:24:14 +0100

    Key Verification: Add model for m.key.verification.request by to_device event
    https://matrix.org/docs/spec/client_server/latest#m-key-verification-request

---
+++ MatrixSDK/Crypto/Verification/Requests/MXKeyVerificationByToDeviceRequest.m
@@ -0,0 +1,80 @@
+/*
+ Copyright 2020 The Matrix.org Foundation C.I.C
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "MXKeyVerificationByToDeviceRequest.h"
+
+#import "MXKeyVerificationRequest_Private.h"
+#import "MXKeyVerificationManager_Private.h"
+#import "MXCrypto_Private.h"
+
+#import "MXEvent.h"
+
+@implementation MXKeyVerificationByToDeviceRequest
+
+- (instancetype)initWithEvent:(MXEvent*)event andManager:(MXKeyVerificationManager*)manager
+{
+    // Check verification by DM request format
+    MXKeyVerificationRequestByToDeviceJSONModel *request;
+    MXJSONModelSetMXJSONModel(request, MXKeyVerificationRequestByToDeviceJSONModel.class, event.content);
+    
+    if (!request)
+    {
+        return nil;
+    }
+    
+    self = [super initWithEvent:event andManager:manager];
+    if (self)
+    {
+        _request = request;
+    }
+    return self;
+}
+
+
+// Shortcuts
+- (NSString *)requestId
+{
+    return _request.transactionId;
+}
+
+- (uint64_t)ageLocalTs
+{
+    return _request.timestamp;
+}
+
+
+// Shortcuts to the original request
+- (NSString *)otherUser
+{
+    return self.isFromMyUser ? self.event.sender : self.manager.crypto.mxSession.myUser.userId;
+}
+
+- (NSString *)otherDevice
+{
+    return self.isFromMyUser ? self.acceptedData.fromDevice : _request.fromDevice;
+}
+
+- (NSString *)fromDevice
+{
+    return _request.fromDevice;
+}
+
+- (NSArray<NSString *> *)methods
+{
+    return _request.methods;
+}
+
+@end
