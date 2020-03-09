ebf1338379d58d96ea812c09bbf1d53893546733
SBiOSoftWhare <steveb@matrix.org>
2020-03-09 19:52:14 +0100

    QR code verification: Add integration tests for another user verification by QR code.

--- MatrixSDKTests/MXCrossSigningVerificationTests.m
+++ MatrixSDKTests/MXCrossSigningVerificationTests.m
@@ -26,6 +26,8 @@
 
 #import "MXKeyVerificationRequestByDMJSONModel.h"
 
+#import "MXQRCodeTransaction_Private.h"
+
 // Do not bother with retain cycles warnings in tests
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-retain-cycles"
@@ -33,6 +35,7 @@
 @interface MXKeyVerificationManager (Testing)
 
 - (MXKeyVerificationTransaction*)transactionWithTransactionId:(NSString*)transactionId;
+- (MXQRCodeTransaction*)qrCodeTransactionWithTransactionId:(NSString*)transactionId;
 
 @end
 
@@ -91,6 +94,24 @@
     [observers addObject:observer];
 }
 
+- (void)observeNewQRCodeTransactionInSession:(MXSession*)session block:(void (^)(MXQRCodeTransaction * _Nullable transaction))block
+{
+    id observer = [[NSNotificationCenter defaultCenter] addObserverForName:MXKeyVerificationManagerNewTransactionNotification object:session.crypto.keyVerificationManager queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *notif) {
+        
+        MXKeyVerificationTransaction *transaction = notif.userInfo[MXKeyVerificationManagerNotificationTransactionKey];
+        if ([transaction isKindOfClass:MXQRCodeTransaction.class])
+        {
+            block((MXQRCodeTransaction*)transaction);
+        }
+        else
+        {
+            XCTFail(@"We support only QR code. transaction: %@", transaction);
+        }
+    }];
+    
+    [observers addObject:observer];
+}
+
 - (void)observeTransactionUpdate:(MXKeyVerificationTransaction*)transaction block:(void (^)(void))block
 {
     id observer = [[NSNotificationCenter defaultCenter] addObserverForName:MXKeyVerificationTransactionDidChangeNotification object:transaction queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *notif) {
@@ -100,18 +121,19 @@
     [observers addObject:observer];
 }
 
-- (void)observeKeyVerificationRequestInSession:(MXSession*)session block:(void (^)(MXKeyVerificationRequest * _Nullable request))block
+- (void)observeKeyVerificationRequestChangeWithBlock:(void (^)(MXKeyVerificationRequest * _Nullable request))block
 {
-    id observer = [[NSNotificationCenter defaultCenter] addObserverForName:MXKeyVerificationManagerNewRequestNotification object:session.crypto.keyVerificationManager queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *notif) {
+    id observer = [[NSNotificationCenter defaultCenter] addObserverForName:MXKeyVerificationRequestDidChangeNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *notif) {
         
-        MXKeyVerificationRequest *request = notif.userInfo[MXKeyVerificationManagerNotificationRequestKey];
+        MXKeyVerificationRequest *request = notif.object;
+        
         if ([request isKindOfClass:MXKeyVerificationRequest.class])
         {
             block((MXKeyVerificationRequest*)request);
         }
         else
         {
-            XCTFail(@"We support only SAS. transaction: %@", request);
+            XCTFail(@"We support only MXKeyVerificationRequest. request: %@", request);
         }
     }];
     
@@ -383,8 +405,251 @@
                                             onEvent:checkDoneDone];
                 [bobSession listenToEventsOfTypes:@[kMXEventTypeStringKeyVerificationDone]
                                           onEvent:checkDoneDone];
+            }];
+        }];
+    }];
+}
+
+/**
+ Verify another user by QR code
+ It reuses code from testVerificationByDMFullFlow from MXCryptoKeyVerificationTests.
+ 
+ - Alice and Bob are in a room
+ - Alice and Bob bootstrap cross-signing (This is the single difference with original testVerificationByDMFullFlow).
+ - Bob requests a verification of Alice in this Room
+ - Alice gets the request in the timeline
+ - Alice accepts it and wait for Bob to scan her QR code
+ -> 1. Transaction on Bob side must be Unknown
+ -> 2. Transaction on Alice side must be Unknown
+ -  Bob scan Alice QR code
+ -> 3. Transaction on Bob side must then move to ScannedOtherQR
+ -> 4. Transaction on Bob side must then move to Verified
+ -> 5. Transaction on Alice side must then move to QRScannedByOther
+ -  Alice confirms that Bob has scanned her QR code
+ -> 6. Transaction on Alice side must then move to Verified
+ -> Users must be verified
+ -> Transaction must not be listed anymore
+ -> Both ends must get a done message
+ - Then, test MXKeyVerification
+ */
+- (void)testVerifyingAnotherUserQRCodeVerificationFullFlow
+{
+    // - Alice and Bob are in a room
+    [matrixSDKTestsE2EData doE2ETestWithAliceAndBobInARoom:self cryptedBob:YES warnOnUnknowDevices:YES aliceStore:[[MXMemoryStore alloc] init] bobStore:[[MXMemoryStore alloc] init] readyToTest:^(MXSession *aliceSession, MXSession *bobSession, NSString *roomId, XCTestExpectation *expectation) {
+        
+        // - Alice and Bob bootstrap cross-signing
+        [self bootstrapCrossSigningOnSession:aliceSession password:MXTESTS_ALICE_PWD completion:^{
+            [self bootstrapCrossSigningOnSession:bobSession password:MXTESTS_BOB_PWD completion:^{
+                
+                NSString *fallbackText = @"fallbackText";
+                __block NSString *requestId;
+                
+                __block MXQRCodeData *aliceQRCodeData;
+                
+                MXCredentials *alice = aliceSession.matrixRestClient.credentials;
+                MXCredentials *bob = bobSession.matrixRestClient.credentials;
+                
+                // - Bob requests a verification of Alice in this Room
+                [bobSession.crypto.keyVerificationManager requestVerificationByDMWithUserId:alice.userId
+                                                                                     roomId:roomId
+                                                                               fallbackText:fallbackText
+                                                                                    methods:@[MXKeyVerificationMethodQRCodeShow, MXKeyVerificationMethodQRCodeScan, MXKeyVerificationMethodReciprocate]
+                                                                                    success:^(MXKeyVerificationRequest *request)
+                 {
+                     requestId = request.requestId;
+                 }
+                                                                                    failure:^(NSError * _Nonnull error)
+                 {
+                     XCTFail(@"The request should not fail - NSError: %@", error);
+                     [expectation fulfill];
+                 }];
+                
+                __block MXQRCodeTransaction *qrCodeTransactionFromAlicePOV;
+                
+                // Alice gets the request in the timeline
+                [aliceSession listenToEventsOfTypes:@[kMXEventTypeStringRoomMessage]
+                                            onEvent:^(MXEvent *event, MXTimelineDirection direction, id customObject)
+                 {
+                     if ([event.content[@"msgtype"] isEqualToString:kMXMessageTypeKeyVerificationRequest])
+                     {
+                         XCTAssertEqualObjects(event.eventId, requestId);
+                         
+                         // Check verification by DM request format
+                         MXKeyVerificationRequestByDMJSONModel *requestJSON;
+                         MXJSONModelSetMXJSONModel(requestJSON, MXKeyVerificationRequestByDMJSONModel.class, event.content);
+                         XCTAssertNotNil(requestJSON);
+                         
+                         // - Alice accepts it and creates a QR code transaction
+                         
+                         // Wait a bit
+                         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+                             // - Alice accepts the incoming request
+                             MXKeyVerificationRequest *requestFromAlicePOV = aliceSession.crypto.keyVerificationManager.pendingRequests.firstObject;
+                             XCTAssertNotNil(requestFromAlicePOV);
+                             
+                             [requestFromAlicePOV acceptWithMethods:@[MXKeyVerificationMethodQRCodeShow, MXKeyVerificationMethodReciprocate] success:^{
+                                 
+                                 qrCodeTransactionFromAlicePOV = [aliceSession.crypto.keyVerificationManager qrCodeTransactionWithTransactionId:requestFromAlicePOV.requestId];
+                                 
+                                 aliceQRCodeData = qrCodeTransactionFromAlicePOV.qrCodeData;
+                                 
+                                 XCTAssertEqual(requestFromAlicePOV.state, MXKeyVerificationRequestStateReady);
+                                 XCTAssertNotNil(aliceQRCodeData);
+                                 XCTAssertEqual(aliceQRCodeData.verificationMode, MXQRCodeVerificationModeVerifyingAnotherUser);                                                                                                   
+                                 
+                             } failure:^(NSError * _Nonnull error) {
+                                 XCTFail(@"The request should not fail - NSError: %@", error);
+                                 [expectation fulfill];
+                             }];
+                         });
+                     }
+                 }];
                 
                 
+                [self observeNewQRCodeTransactionInSession:bobSession block:^(MXQRCodeTransaction * _Nullable qrCodeTransactionFromBobPOV) {
+                    
+                    // Final checks
+                    void (^checkBothDeviceVerified)(void) = ^ void ()
+                    {
+                        if (qrCodeTransactionFromAlicePOV.state == MXQRCodeTransactionStateVerified
+                            && qrCodeTransactionFromBobPOV.state == MXQRCodeTransactionStateVerified)
+                        {
+                            // -> Users must be verified
+                            MXDeviceInfo *bobDeviceFromAlicePOV = [aliceSession.crypto.store deviceWithDeviceId:bob.deviceId forUser:bob.userId];
+                            MXDeviceInfo *aliceDeviceFromBobPOV = [bobSession.crypto.store deviceWithDeviceId:alice.deviceId forUser:alice.userId];
+                            
+                            XCTAssertTrue(bobDeviceFromAlicePOV.trustLevel.isCrossSigningVerified);
+                            XCTAssertTrue(aliceDeviceFromBobPOV.trustLevel.isCrossSigningVerified);
+                            
+                            // -> Transaction must not be listed anymore
+                            XCTAssertNil([aliceSession.crypto.keyVerificationManager transactionWithTransactionId:qrCodeTransactionFromAlicePOV.transactionId]);
+                            XCTAssertNil([bobSession.crypto.keyVerificationManager transactionWithTransactionId:qrCodeTransactionFromBobPOV.transactionId]);
+                        }
+                    };
+                    
+                    // -> Transaction on Alice side must be Unknown, then QRScannedByOther
+                    [self observeTransactionUpdate:qrCodeTransactionFromAlicePOV block:^{
+                        
+                        switch (qrCodeTransactionFromAlicePOV.state)
+                        {
+                                // -> 2. Transaction on Alice side must be Unknown
+                            case MXQRCodeTransactionStateUnknown:
+                                XCTAssertEqual(qrCodeTransactionFromBobPOV.state, MXQRCodeTransactionStateUnknown);
+                                break;
+                                // -> 5. Transaction on Alice side must then move to QRScannedByOther
+                            case MXQRCodeTransactionStateQRScannedByOther:
+                                XCTAssertEqual(qrCodeTransactionFromBobPOV.state, MXQRCodeTransactionStateScannedOtherQR);
+                                
+                                // Alice confirms that Bob has scanned her QR code
+                                [qrCodeTransactionFromAlicePOV otherUserScannedMyQrCode:YES];
+                                break;
+                                // -> 6. Transaction on Alice side must then move to Verified
+                            case MXQRCodeTransactionStateVerified:
+                                checkBothDeviceVerified();
+                                break;
+                            default:
+                                XCTAssert(NO, @"Unexpected Alice transation state: %@", @(qrCodeTransactionFromAlicePOV.state));
+                                break;
+                        }
+                    }];
+                    
+                    // -> Transaction on Bob side must be Unknown, then ScannedOtherQR
+                    [self observeTransactionUpdate:qrCodeTransactionFromBobPOV block:^{
+                        
+                        switch (qrCodeTransactionFromBobPOV.state)
+                        {
+                            // -> 1. Transaction on Bob side must be Unknown (Alice is Unknown)
+                            case MXQRCodeTransactionStateUnknown:
+                                XCTAssertEqual(qrCodeTransactionFromAlicePOV.state, MXSASTransactionStateOutgoingWaitForPartnerToAccept);
+                                break;
+                            // -> 3. Transaction on Bob side must then move to ScannedOtherQR
+                            case MXQRCodeTransactionStateScannedOtherQR:
+                                XCTAssertEqual(qrCodeTransactionFromAlicePOV.state, MXQRCodeTransactionStateUnknown);
+                                break;
+                            // -> 4. Transaction on Bob side must then move to Verified
+                            case MXQRCodeTransactionStateVerified:
+                                checkBothDeviceVerified();
+                                break;
+                            default:
+                                XCTAssert(NO, @"Unexpected Bob transation state: %@", @(qrCodeTransactionFromAlicePOV.state));
+                                break;
+                        }
+                    }];
+                }];
+                
+                // -> Both ends must get a done message
+                NSMutableArray<MXKeyVerificationDone*> *doneDone = [NSMutableArray new];
+                void (^checkDoneDone)(MXEvent *event, MXTimelineDirection direction, id customObject) = ^ void (MXEvent *event, MXTimelineDirection direction, id customObject)
+                {
+                    XCTAssertEqual(event.eventType, MXEventTypeKeyVerificationDone);
+                    
+                    // Check done format
+                    MXKeyVerificationDone *done;
+                    MXJSONModelSetMXJSONModel(done, MXKeyVerificationDone.class, event.content);
+                    XCTAssertNotNil(done);
+                    
+                    [doneDone addObject:done];
+                    if (doneDone.count == 2)
+                    {
+                        [expectation fulfill];
+                        
+//                        // Then, to test MXKeyVerification, remove transaction removal code in -[MXQRCodeTransaction sendVerified]
+//                        MXEvent *event = [aliceSession.store eventWithEventId:requestId inRoom:roomId];
+//                        [aliceSession.crypto.keyVerificationManager keyVerificationFromKeyVerificationEvent:event success:^(MXKeyVerification * _Nonnull verificationFromAlicePOV) {
+//
+//                            XCTAssertEqual(verificationFromAlicePOV.state, MXKeyVerificationStateVerified);
+//
+//                            [expectation fulfill];
+//                        } failure:^(NSError * _Nonnull error) {
+//                            XCTFail(@"The request should not fail - NSError: %@", error);
+//                            [expectation fulfill];
+//                        }];
+                    }
+                };
+                
+                [aliceSession listenToEventsOfTypes:@[kMXEventTypeStringKeyVerificationDone]
+                                            onEvent:checkDoneDone];
+                [bobSession listenToEventsOfTypes:@[kMXEventTypeStringKeyVerificationDone]
+                                          onEvent:checkDoneDone];
+                
+                // -> Bob gets the requests notification
+                [self observeKeyVerificationRequestChangeWithBlock:^(MXKeyVerificationRequest * _Nullable request) {
+                    
+                    if (!request.isFromMyUser)
+                    {
+                        return;
+                    }
+                    
+                    XCTAssertEqualObjects(request.requestId, requestId);
+                    XCTAssertTrue(request.isFromMyUser);
+                    
+                    MXKeyVerificationRequest *requestFromAlicePOV = aliceSession.crypto.keyVerificationManager.pendingRequests.firstObject;
+                    MXKeyVerificationRequest *requestFromBobPOV = bobSession.crypto.keyVerificationManager.pendingRequests.firstObject;
+                    
+                    XCTAssertNotNil(requestFromAlicePOV);
+                    XCTAssertEqual(requestFromAlicePOV.transport, MXKeyVerificationTransportDirectMessage);
+                    XCTAssertNotNil(requestFromBobPOV);
+                    XCTAssertEqual(requestFromBobPOV.transport, MXKeyVerificationTransportDirectMessage);
+                    
+                    switch (request.state)
+                    {
+                        case MXKeyVerificationRequestStateReady:
+                        {
+                            MXQRCodeTransaction *qrCodeTransactionFromBobPOV = [bobSession.crypto.keyVerificationManager qrCodeTransactionWithTransactionId:request.requestId];
+                            XCTAssertNotNil(qrCodeTransactionFromBobPOV);
+                            XCTAssertNil(qrCodeTransactionFromBobPOV.qrCodeData); // Bob cannot show QR code
+                            
+                            // Bob scan Alice QR code
+                            [qrCodeTransactionFromBobPOV userHasScannedOtherQrCodeData:aliceQRCodeData];
+                        }
+                            break;
+                        case MXKeyVerificationRequestStateCancelled:
+                            XCTFail(@"The request should not be cancel - Cancel code: %@", request.reasonCancelCode);
+                            break;
+                        default:
+                            break;
+                    }
+                }];
             }];
         }];
     }];
