6185ae9f1d6ad9fc518562edaeddb54487ac4bb1
SBiOSoftWhare <steveb@matrix.org>
2020-03-09 19:21:17 +0100

    QR code verification: Add MXQRCodeDataCoder unit tests.

---
+++ MatrixSDKTests/MXQRCodeDataTests.m
@@ -0,0 +1,71 @@
+/*
+ Copyright 2020 The Matrix.org Foundation C.I.C
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <XCTest/XCTest.h>
+
+#import "MXQRCodeDataCoder.h"
+#import "MXQRCodeDataBuilder.h"
+#import "MXVerifyingAnotherUserQRCodeData.h"
+
+@interface MXQRCodeDataTests : XCTestCase
+
+@end
+
+@implementation MXQRCodeDataTests
+
+- (void)setUp
+{
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testDecode
+{
+    MXQRCodeVerificationMode expectedVerificationMode = MXQRCodeVerificationModeVerifyingAnotherUser;
+    NSString *expectedTransactionId = @"$zoeruzeprupzouerpzeupir";
+    NSString *expectedFirstKey = @"ktEwcUP6su1xh+GuE+CYkQ3H6W/DIl+ybHFdaEOrolU";
+    NSString *expectedSecondKey = @"TXluZKTZLvSRWOTPlOqLq534bA+/K4zLFKSu9cGLQaU";
+    NSData *expectedSharedSecret = [@"MTIzNDU2Nzg" dataUsingEncoding:NSASCIIStringEncoding];
+    
+    MXQRCodeDataBuilder *qrCodeDataBuilder = [MXQRCodeDataBuilder new];
+    MXQRCodeDataCoder *qrCodeDataCoder = [MXQRCodeDataCoder new];
+    
+    MXQRCodeData *qrCodeData = [qrCodeDataBuilder buildQRCodeDataWithVerificationMode:expectedVerificationMode
+                                                                        transactionId:expectedTransactionId
+                                                                             firstKey:expectedFirstKey
+                                                                            secondKey:expectedSecondKey
+                                                                         sharedSecret:expectedSharedSecret];
+    
+    NSData *qrCodeRawData = [qrCodeDataCoder encode:qrCodeData];
+    
+    XCTAssertNotNil(qrCodeRawData);
+        
+    MXQRCodeData *decodedQRCodeData = [qrCodeDataCoder decode:qrCodeRawData];
+    
+    XCTAssertNotNil(decodedQRCodeData);
+    XCTAssertTrue([decodedQRCodeData isKindOfClass:MXVerifyingAnotherUserQRCodeData.class]);
+    XCTAssertEqual(decodedQRCodeData.verificationMode, expectedVerificationMode);
+    XCTAssertEqualObjects(decodedQRCodeData.transactionId, expectedTransactionId);
+    XCTAssertEqualObjects(decodedQRCodeData.firstKey, expectedFirstKey);
+    XCTAssertEqualObjects(decodedQRCodeData.secondKey, expectedSecondKey);
+    XCTAssertEqualObjects(decodedQRCodeData.sharedSecret, expectedSharedSecret);
+}
+
+@end
