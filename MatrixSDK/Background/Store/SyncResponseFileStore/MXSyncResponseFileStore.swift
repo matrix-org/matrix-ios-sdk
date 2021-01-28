52e73eb8ddb3d8be85965265de625cae4fb62ce2
manuroe <manu@matrix.org>
2021-01-28 15:21:40 +0100

    Background sync: Add memory logging

--- MatrixSDK/Background/Store/SyncResponseFileStore/MXSyncResponseFileStore.swift
+++ MatrixSDK/Background/Store/SyncResponseFileStore/MXSyncResponseFileStore.swift
@@ -70,15 +70,22 @@ public class MXSyncResponseFileStore: NSObject {
         fileOperationQueue.sync {
             fileContents = try? String(contentsOf: filePath,
                                        encoding: Constants.fileEncoding)
-            NSLog("[MXSyncResponseFileStore] readData: File read lasted \(stopwatch.readable())")
+            NSLog("[MXSyncResponseFileStore] readData: File read of \(fileContents?.count ?? 0) bytes lasted \(stopwatch.readable()). Free memory: \(MXMemory.formattedMemoryAvailable())")
+            
         }
+        
+        stopwatch.reset()
         guard let jsonString = fileContents else {
             return nil
         }
         guard let json = MXTools.deserialiseJSONString(jsonString) as? [AnyHashable: Any] else {
             return nil
         }
-        return MXSyncResponseStoreModel(fromJSON: json)
+        
+        let model = MXSyncResponseStoreModel(fromJSON: json)
+        
+        NSLog("[MXSyncResponseFileStore] readData: Consersion to model lasted \(stopwatch.readable()). Free memory: \(MXMemory.formattedMemoryAvailable())")
+        return model
     }
     
     private func saveData(_ data: MXSyncResponseStoreModel?) {
@@ -97,7 +104,7 @@ public class MXSyncResponseFileStore: NSObject {
             try? data.jsonString()?.write(to: self.filePath,
                                           atomically: true,
                                           encoding: Constants.fileEncoding)
-            NSLog("[MXSyncResponseFileStore] saveData: File write lasted \(stopwatch.readable())")
+            NSLog("[MXSyncResponseFileStore] saveData: File write lasted \(stopwatch.readable()). Free memory: \(MXMemory.formattedMemoryAvailable())")
         }
     }
     
