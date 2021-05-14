62adc8a16dae05adb1b263575dd8bfcca7cd6434
ismailgulek <ismailg@matrix.org>
2021-05-14 17:55:29 +0300

    Implement store

--- MatrixSDK/Data/RoomSummaryStore/MXRoomSummaryCoreDataStore/MXRoomSummaryCoreDataStore.swift
+++ MatrixSDK/Data/RoomSummaryStore/MXRoomSummaryCoreDataStore/MXRoomSummaryCoreDataStore.swift
@@ -18,20 +18,99 @@ import Foundation
 import CoreData
 
 @objcMembers
-public class MXRoomSummaryCoreDataStore {
+public class MXRoomSummaryCoreDataStore: NSObject {
     
-    private var managedContext: NSManagedObjectContext = NSManagedObjectContext()
+    private enum Constants {
+        static let modelName: String = "MXRoomSummaryStore"
+        static let folderName: String = "RoomSummaryStore"
+        static let storeFileName: String = "RoomSummaryStore.sqlite"
+    }
+    
+    private let credentials: MXCredentials
+    
+    private lazy var storeURL: URL = {
+        guard let userId = credentials.userId else {
+            fatalError("Credentials must provide a user identifier")
+        }
+        
+        var cachePath: URL!
+        if let appGroupIdentifier = MXSDKOptions.sharedInstance().applicationGroupIdentifier {
+            cachePath = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroupIdentifier)
+        } else {
+            cachePath = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first
+        }
+        let folderUrl = cachePath.appendingPathComponent(Constants.folderName).appendingPathComponent(userId)
+        try? FileManager.default.createDirectory(at: folderUrl, withIntermediateDirectories: true, attributes: nil)
+        return folderUrl.appendingPathComponent(Constants.storeFileName)
+    }()
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        guard let url = Bundle(for: MXRoomSummaryModel.self).url(forResource: Constants.modelName,
+                                                                 withExtension: "momd") else {
+            fatalError("No MXRoomSummaryStore Core Data model")
+        }
+        guard let result = NSManagedObjectModel(contentsOf: url) else {
+            fatalError("Cannot create managed object model")
+        }
+        return result
+    }()
+    private lazy var persistenceCoordinator: NSPersistentStoreCoordinator = {
+        let result = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+        do {
+            try result.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+        return result
+    }()
+    private lazy var managedObjectContext: NSManagedObjectContext = {
+        let result = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        result.persistentStoreCoordinator = persistenceCoordinator
+        return result
+    }()
+    
+    public init(withCredentials credentials: MXCredentials) {
+        self.credentials = credentials
+        super.init()
+    }
+    
+    private func fetchSummary(forRoomId roomId: String) -> MXRoomSummaryModel? {
+        let request = MXRoomSummaryModel.typedFetchRequest()
+        request.predicate = NSPredicate(format: "identifier == %@", roomId)
+        do {
+            let results = try managedObjectContext.fetch(request)
+            return results.first
+        } catch {
+            NSLog("[MXRoomSummaryCoreDataStore] fetchSummary failed: \(error)")
+        }
+        return nil
+    }
     
 }
 
 extension MXRoomSummaryCoreDataStore: MXRoomSummaryStore {
     
-    public func storeRoomSummary(_ roomSummary: MXRoomSummary, forRoomId roomId: String) {
-        
+    public func storeSummary(forRoom roomId: String, summary: MXRoomSummary) {
+        if let existing = fetchSummary(forRoomId: roomId) {
+            existing.update(withRoomSummary: summary, in: managedObjectContext)
+        } else {
+            let model = MXRoomSummaryModel.from(roomSummary: summary, in: managedObjectContext)
+            managedObjectContext.insert(model)
+        }
+
+        do {
+            if managedObjectContext.hasChanges {
+                try managedObjectContext.save()
+            }
+        } catch {
+            NSLog("[MXRoomSummaryCoreDataStore] storeSummary failed: \(error)")
+        }
     }
     
-    public func roomSummary(forRoomId roomId: String) -> MXRoomSummary? {
-        return nil
+    public func summary(ofRoom roomId: String) -> MXRoomSummary? {
+        guard let result = fetchSummary(forRoomId: roomId) else {
+            return nil
+        }
+        return MXRoomSummary(coreDataModel: result)
     }
     
 }
