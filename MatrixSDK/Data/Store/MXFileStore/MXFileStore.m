b9d444aa3646ac657fa8dba87a6def9ecc73f4ba
SBiOSoftWhare <steveb@matrix.org>
2021-07-22 08:42:18 +0200

    Update file store version.

--- MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
+++ MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
@@ -26,7 +26,7 @@
 #import "MXSDKOptions.h"
 #import "MXTools.h"
 
-static NSUInteger const kMXFileVersion = 73;
+static NSUInteger const kMXFileVersion = 74;
 
 static NSString *const kMXFileStoreFolder = @"MXFileStore";
 static NSString *const kMXFileStoreMedaDataFile = @"MXFileStore";
