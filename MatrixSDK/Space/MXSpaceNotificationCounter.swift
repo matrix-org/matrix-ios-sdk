ae8816791c0b7d25233a39eafad3ac807a6e4561
Gil Eluard <gile@element.io>
2021-09-23 10:25:44 +0200

    Updated header and comments in MXSpaceNotificationCounter.swift

--- MatrixSDK/Space/MXSpaceNotificationCounter.swift
+++ MatrixSDK/Space/MXSpaceNotificationCounter.swift
@@ -1,23 +1,41 @@
 //
-//  MXSpaceNotificationCounter.swift
-//  MatrixSDK
+// Copyright 2021 The Matrix.org Foundation C.I.C
 //
-//  Created by Gil Eluard on 19/09/2021.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 import Foundation
 
+/// MXSpaceNotificationState stores the categorized number of unread messages
 @objcMembers
 public class MXSpaceNotificationState: NSObject {
+    /// Number of unread messages in favourite rooms
     public var favouriteMissedDiscussionsCount: UInt = 0
+    /// Number of unread highlight messages in favourite rooms
     public var favouriteMissedDiscussionsHighlightedCount: UInt = 0
+    /// Number of unread messages in DM rooms
     public var directMissedDiscussionsCount: UInt = 0
+    /// Number of unread highlight messages in DM rooms
     public var directMissedDiscussionsHighlightedCount: UInt = 0
+    /// Number of unread messages in rooms other than DMs
     public var groupMissedDiscussionsCount: UInt = 0
+    /// Number of unread highlight message sin rooms other than DMs
     public var groupMissedDiscussionsHighlightedCount: UInt = 0
+    /// Number of all unread messages
     public var allCount: UInt {
         return favouriteMissedDiscussionsCount + directMissedDiscussionsCount + groupMissedDiscussionsCount
     }
+    /// Number of all unread highlight messages
     public var allHighlightCount: UInt {
         return favouriteMissedDiscussionsHighlightedCount + directMissedDiscussionsHighlightedCount + groupMissedDiscussionsHighlightedCount
     }
@@ -38,6 +56,7 @@ public class MXSpaceNotificationState: NSObject {
     }
 }
 
+/// MXSpaceNotificationCounter compute the number of unread messages for each space
 @objcMembers
 public class MXSpaceNotificationCounter: NSObject {
     
