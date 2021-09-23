1ac2d2339518f6b7690eaf133b71e67beb51f61e
ismailgulek <ismailg@matrix.org>
2021-09-23 16:42:46 +0300

    Add new properties and refactor

--- MatrixSDK/Data/RoomList/MXRoomListData.swift
+++ MatrixSDK/Data/RoomList/MXRoomListData.swift
@@ -43,14 +43,34 @@ public final class MXRoomListData: NSObject {
         return rooms.count
     }
     
+    /// Number of rooms having unsent message(s)
+    public var numberOfUnsentRooms: Int {
+        return rooms.filter({ $0.sentStatus != .ok }).count
+    }
+    
+    /// Number of rooms being notified
+    public var numberOfNotifiedRooms: Int {
+        return rooms.filter({ $0.notificationCount > 0 }).count + numberOfInvitedRooms
+    }
+    
+    /// Number of room being highlighted
+    public var numberOfHighlightedRooms: Int {
+        return rooms.filter({ $0.highlightCount > 0 }).count
+    }
+    
     /// Total notification count for handled rooms
-    public var notificationCount: UInt {
+    public var totalNotificationCount: UInt {
         return rooms.reduce(0, { $0 + $1.notificationCount })
     }
     
     /// Total highlight count for handled rooms
-    public var highlightCount: UInt {
+    public var totalHighlightCount: UInt {
         return rooms.reduce(0, { $0 + $1.highlightCount })
+    }
+    
+    /// Number of invited rooms
+    private var numberOfInvitedRooms: Int {
+        return rooms.filter({ $0.isTyped(.invited) }).count
     }
     
     /// Get room at index
