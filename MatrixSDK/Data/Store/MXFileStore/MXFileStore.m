54be5ca72523bbd7f8e23e40e6de4599d0e1fed5
ismailgulek <ismailg@matrix.org>
2022-01-07 16:05:00 +0300

    Increment file store version

--- MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
+++ MatrixSDK/Data/Store/MXFileStore/MXFileStore.m
@@ -29,7 +29,7 @@
 #import "MatrixSDKSwiftHeader.h"
 #import "MXFileRoomSummaryStore.h"
 
-static NSUInteger const kMXFileVersion = 77;
+static NSUInteger const kMXFileVersion = 78;
 
 static NSString *const kMXFileStoreFolder = @"MXFileStore";
 static NSString *const kMXFileStoreMedaDataFile = @"MXFileStore";
