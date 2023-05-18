e1c9e5ac0780d627690c1914bb76e642bfb3bc04
David Langley <langley.dave@gmail.com>
2023-05-18 16:36:06 +0100

    allow empty array for chunk in MXAggregationPaginatedResponse

--- MatrixSDK/JSONModels/Aggregations/MXAggregationPaginatedResponse.m
+++ MatrixSDK/JSONModels/Aggregations/MXAggregationPaginatedResponse.m
@@ -35,12 +35,18 @@
     MXAggregationPaginatedResponse *paginatedResponse;
 
     NSArray<MXEvent*> *chunk;
-    MXJSONModelSetMXJSONModelArray(chunk, MXEvent.class, JSONDictionary[@"chunk"])
-
+    NSArray *chunkJson = JSONDictionary[@"chunk"];
+    MXJSONModelSetMXJSONModelArray(chunk, MXEvent.class, chunkJson)
+    // For some reason modelsFromJSON returns nil if you pass it an empty array.
+    // In this case we want an empty array or we get an error.
+    if(!chunk && [chunkJson isKindOfClass:NSArray.class])
+    {
+        chunk = @[];
+    }
+    
     if (chunk)
     {
         paginatedResponse = [MXAggregationPaginatedResponse new];
-
         paginatedResponse->_chunk = chunk;
         MXJSONModelSetString(paginatedResponse->_nextBatch, JSONDictionary[@"next_batch"])
 
