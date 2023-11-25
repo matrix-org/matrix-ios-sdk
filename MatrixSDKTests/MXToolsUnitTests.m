c038a67330ca6a2efd2fc247a9f68fd2fbffe494
yostyle <y.pintas@gmail.com>
2023-11-25 02:47:23 +0100

    Add unit test

--- MatrixSDKTests/MXToolsUnitTests.m
+++ MatrixSDKTests/MXToolsUnitTests.m
@@ -68,6 +68,58 @@
     XCTAssertTrue([MXTools isMatrixGroupIdentifier:@"+matrix:matrix.org"]);
 }
 
+- (void)testEmailAddresses
+{
+    XCTAssertTrue([MXTools isEmailAddress:@"alice@matrix.org"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"alice@matrix"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"al-i_ce@matrix"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"al+ice@matrix.org"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"al=ice@matrix.org"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"*@example.net"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"fred&barny@example.com"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"---@example.com"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"foo-bar@example.net"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"mailbox.sub1.sub2@this-domain"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"prettyandsimple@example.com"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"very.common@example.com"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"disposable.style.email.with+symbol@example.com"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"other.email-with-dash@example.com"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"x@example.com"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"example-indeed@strange-example.com"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"admin@mailserver1"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"#!$%&'*+-/=?^_`{}|~@example.org"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"example@localhost"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"example@s.solutions"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"user@localserver"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"user@tt"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"xn--80ahgue5b@xn--p-8sbkgc5ag7bhce.xn--ba-lmcq"]);
+    XCTAssertTrue([MXTools isEmailAddress:@"nothing@xn--fken-gra.no"]);
+    
+    XCTAssertFalse([MXTools isEmailAddress:@"alice.matrix.org"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"al ice@matrix.org"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"al(ice@matrix.org"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"alice@"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"al\nice@matrix.org"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"al@ice@matrix.org"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"al@ice@.matrix.org"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"Just a string"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"string"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"me@"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"@example.com"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"me.@example.com"]);
+    XCTAssertFalse([MXTools isEmailAddress:@".me@example.com"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"me@example..com"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"me\\@example.com"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"Abc.example.com"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"A@b@c@example.com"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"a\"b(c)d,e:f;g<h>i[j\\k]l@example.com"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"just\"not\"right@example.com"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"this is\"not\\allowed@example.com"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"this\\ still\\\"not\\\\allowed@example.com"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"john..doe@example.com"]);
+    XCTAssertFalse([MXTools isEmailAddress:@"john.doe@example..com"]);
+}
+
 
 #pragma mark - Strings encoding
 
